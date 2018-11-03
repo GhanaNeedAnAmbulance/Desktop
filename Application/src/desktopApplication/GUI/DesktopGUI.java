66775cc9640d660ada397476e97dc102ecbf2c7e
Benjamin Johnson <Benjamin Johnson@10.25.195.171>
2018-11-03 14:14:53 -0400

    GUI 2nd Draft

--- Application/src/desktopApplication/GUI/DesktopGUI.java
+++ Application/src/desktopApplication/GUI/DesktopGUI.java
@@ -33,18 +33,19 @@ public class DesktopGUI extends Application {
 		Button closeButton = new Button("Cancel");
 		ButtonBar buttons = new ButtonBar();
 		buttons.getButtons().addAll(applyButton,saveButton,closeButton);
+		buttons.setButtonMinWidth(90);
 		
 		Text hName = new Text(hospitalName);
 		hName.setFont(Font.font("Helvetica",20));
 		Text avaBedsText = new Text("Available Beds:");
 		avaBedsText.setFont(Font.font("Helvetica",14));
 		TextField avaBedsField = new TextField();
-		avaBedsField.setFont(Font.font("Helvetica",14));
+		avaBedsField.setFont(Font.font("Helvetica",10));
 		avaBedsField.setMaxWidth(50);
 		Text totBedsText = new Text("Total Beds:");
 		totBedsText.setFont(Font.font("Helvetica",14));
 		TextField totBedsField = new TextField();
-		totBedsField.setFont(Font.font("Helvetica",14));
+		totBedsField.setFont(Font.font("Helvetica",10));
 		totBedsField.setMaxWidth(50);
 		
 		HBox avaBeds = new HBox();
@@ -69,29 +70,27 @@ public class DesktopGUI extends Application {
 	    RowConstraints row1 = new RowConstraints();
 	    row1.setPercentHeight(16);
 	    RowConstraints row2 = new RowConstraints();
-	    row2.setPercentHeight(16);
+	    row2.setPercentHeight(24);
 	    RowConstraints row3 = new RowConstraints();
 	    row3.setPercentHeight(18);
 	    RowConstraints row4 = new RowConstraints();
-	    row4.setPercentHeight(18);
+	    row4.setPercentHeight(16);
 	    RowConstraints row5 = new RowConstraints();
-	    row5.setPercentHeight(16);
-	    RowConstraints row6 = new RowConstraints();
-	    row6.setPercentHeight(16);
-	    root.getRowConstraints().addAll(row1,row2,row3,row4,row5,row6);
+	    row5.setPercentHeight(26);
+	    root.getRowConstraints().addAll(row1,row2,row3,row4,row5);
 	    
 	    // Alignments
 	    GridPane.setHalignment(buttons, HPos.RIGHT);
 	    GridPane.setValignment(buttons, VPos.BOTTOM);
 	    GridPane.setValignment(hName, VPos.TOP);
-	    root.setPadding(new Insets(0,5,0,5));
+	    root.setPadding(new Insets(0,5,5,5));
 	    root.add(hName, 0, 0);
 	    root.add(avaBeds, 0, 2, 2, 1);
 	    root.add(totBeds, 2, 2, 2, 1);
-		root.add(buttons, 3, 5);
+		root.add(buttons, 3, 4);
 		
 		
-		scene = new Scene(root,500,300);
+		scene = new Scene(root,320,100);
 		appStage.setScene(scene);
 		appStage.setTitle("Ghana need an ambulance");
 		appStage.show();
